13040129a5591f316ef42f0f2b12026881350426
Alexey Subbotkin <subbotkin147@gmail.com>
2026-05-19 16:30:55 +0300

    Release 4.0.0-a16-aloha09

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "VLCKit",
-                      url: "https://doloto.alohabrowser.com/repository/maven-releases/com/alohamobile/vlckit-ios/4.0.0-a16-aloha08/vlckit-ios-4.0.0-a16-aloha08-vlckit-ios.zip",
-                      checksum: "5efb6a8400639ab82d22474f840e6a48240dcf6048cb76f6cd0e44f0815049a2"),
+                      url: "https://doloto.alohabrowser.com/repository/maven-releases/com/alohamobile/vlckit-ios/4.0.0-a16-aloha09/vlckit-ios-4.0.0-a16-aloha09-vlckit-ios.zip",
+                      checksum: "537d17eceae35472a435cd54d508c06b1ebd19842172f96b1e777d7fc1f99b81"),
     ]
 )
